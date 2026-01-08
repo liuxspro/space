ae2f86d7b9b23ae023d272eef2c8ff0c95d7e968
liuxspro <liuxspro@gmail.com>
2026-01-08 22:34:45 +0800

    Chore: 添加允许的图片Host

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [new URL("https://image.liuxs.pro/**")],
+  },
 };
 
 export default withMDX(config);
