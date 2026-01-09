b053c8f96d82fa6afbfdc88361e803a5e0511c20
liuxspro <liuxspro@gmail.com>
2026-01-09 14:06:01 +0800

    Chore: add metadataBase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
+import type { Metadata } from "next";
 import "./global.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://space.liuxs.pro"),
+};
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
