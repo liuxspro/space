7c2a2081c61bbdf23762c108994ef611d2d57119
liuxspro <liuxspro@gmail.com>
2026-01-09 14:44:27 +0800

    Chore: 调整图标

--- src/lib/custom_icons.tsx
+++ src/lib/custom_icons.tsx
@@ -1,7 +1,11 @@
-export const TiandituLogo = () => <img src="/icons/tianditu_logo.svg" width={20} alt="" />;
-export const QGISLogo = () => <img src="/icons/qgis_logo.svg" width={20} alt="" />;
+const TiandituLogo = () => <img src="/icons/tianditu_logo.svg" width={20} alt="" />;
+const QGISLogo = () => <img src="/icons/qgis_logo.svg" width={20} alt="" />;
+const Greenbook = () => <img src="/icons/FxemojiGreenbook.svg" width={20} alt="" />;
+const IonEarth = () => <img src="/icons/IonEarth.svg" width={20} alt="" />;
 
 export const customIcons = {
   TiandituLogo,
   QGISLogo,
+  Greenbook,
+  IonEarth,
 };
