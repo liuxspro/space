7c79d7b039ba87efca95d32c500a35fb1c1fe7fd
liuxspro <liuxspro@gmail.com>
2026-01-09 11:42:10 +0800

    Feat: 增加文章筛选

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,15 +1,27 @@
 import { docs } from "fumadocs-mdx:collections/server";
-import { type InferPageType, loader } from "fumadocs-core/source";
+import { type InferPageType, loader, update } from "fumadocs-core/source";
 import { lucideIconsPlugin } from "fumadocs-core/source/lucide-icons";
 import { customIcons } from "./custom_icons";
 import { icons } from "lucide-react";
 import { createElement } from "react";
 import { i18n } from "@/lib/i18n";
 
+// 筛选掉 draft 文章
+const filtered_source = update(docs.toFumadocsSource())
+  .files((files) =>
+    files.filter((file) => {
+      // keep meta files (e.g. `meta.json`)
+      if (file.type === "meta") return true;
+      // access file data (type-safe)
+      return file.data.draft === false;
+    })
+  )
+  .build();
+
 // See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
   baseUrl: "/docs",
-  source: docs.toFumadocsSource(),
+  source: filtered_source,
   plugins: [lucideIconsPlugin()],
   icon(icon) {
     if (icon && icon in customIcons) {
