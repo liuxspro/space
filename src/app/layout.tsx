529f6dbcc987a48e53658a7fbe6267265628b4f1
liuxspro <liuxspro@gmail.com>
2026-01-12 10:40:59 +0800

    Chore: 改为中文UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,17 @@ import { RootProvider } from "fumadocs-ui/provider/next";
 import type { Metadata } from "next";
 import "./global.css";
 import { Inter } from "next/font/google";
+import type { Translations } from "fumadocs-ui/i18n";
 
 const inter = Inter({
   subsets: ["latin"],
 });
+
+const cnTranslations: Partial<Translations> = {
+  search: "搜索",
+  toc: "目录",
+  searchNoResult: "无搜索结果",
+};
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://space.liuxs.pro"),
@@ -15,7 +22,14 @@ export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="zh-CN" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider
+          i18n={{
+            locale: "cn",
+            translations: { cn: cnTranslations }["cn"],
+          }}
+        >
+          {children}
+        </RootProvider>
       </body>
     </html>
   );
